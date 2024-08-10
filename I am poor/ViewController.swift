b10bbc7ad2660d0bd36508018c871566bb8b1399
charlesdamore <charliedamore96@gmail.com>
2024-08-10 16:45:09 +0100

    first commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Charlie Damore on 02/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
